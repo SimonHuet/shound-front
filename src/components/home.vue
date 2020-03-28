7b8ef1d2781a00830c115be7af1e7c7fc917e57b
Maxime Garry <maximegarry@gmail.com>
2020-03-28 14:48:50 +0100

    Feature/front update (#4)
    * mise à jour des routes et de l'affichage
    
    * ajout de cypress
    
    * ajout de eslint
    
    * review/code review with potter
    
    * update var env + api usages
    
    * fix():vulnerabilities
    
    * wip - test unitaires
    
    * Revert "wip - test unitaires"
    
    Revert last commit
    
    * add tests
    
    * feat(): Adding tests to ci
    
    Co-authored-by: SimonHuet <huetsimon35@gmail.com>
    Co-authored-by: Maxime Garry <maxime.garry@sowesign.com>

--- src/components/home.vue
+++ src/components/home.vue
@@ -1,73 +1,72 @@
 <template>
   <div class="home">
-    <h1>{{title}}</h1>
+    <h1> Liste des utilisateurs</h1>
 
     <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">Nom</th>
-      <th scope="col">Ville</th>
-      <th scope="col">Total</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr v-for="(data,index) in customers" :key="index"  @click="goTodetail(data.id)">
-      <th scope="row">{{data.id}}</th>
-      <td>{{data.name}}</td>
-      <td>{{data.city}}</td>
-      <td>{{data.total}}</td>
-    </tr>
-  </tbody>
-</table>
+      <thead>
+        <tr>
+          <th scope="col">Nom</th>
+          <th scope="col">Prénom</th>
+          <th scope="col">Email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <!-- Si tu utilise plusieurs fois un index (1,2,3) en key avec plusieurs v-for dans la même page,
+    tu va avoir des conflit de clé. Essaye de concatété ton index avec une string ('customers' + index par ex )
+     -->
+        <tr v-for="(data,index) in customers" :key="'customers' + index" @click="goTodetail(data.id)">
+          <td>{{data.firstName}}</td>
+          <td>{{data.lastName}}</td>
+          <td>{{data.email}}</td>
+        </tr>
+
+      </tbody>
+    </table>
 
   </div>
 </template>
 
 <script>
 
-const tableData = [];
+import global from '../definitions/global.js'
 
 export default {
   name: 'home',
   data () {
     return {
-      title: 'Liste des utilisateurs',
-      customers: [...tableData]
+      customers: []
     }
   },
-  mounted() {
+  mounted () {
     this.$http
-      .get("http://localhost:3000/customers")
+      .get(url + "/users")
       .then(response => {
         this.customers = response.data;
-        console.log(response.data)
       })
       .catch(error => {
         console.log(error);
       });
-		
+
   },
-  methods:{
-  goTodetail(id) {
-    this.$router.push({name:'orders',params:{Pid:id}})
+  methods: {
+    goTodetail (id) {
+      this.$router.push({ name: 'orders', params: { Pid: id } })
+    }
   }
-}
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.row img{
+.row img {
   max-height: 15em;
   width: 100%;
-
 }
-.row h3{
-  cursor:pointer;
+.row h3 {
+  cursor: pointer;
 }
 
 .table {
-  cursor:pointer;
+  cursor: pointer;
 }
 </style>
